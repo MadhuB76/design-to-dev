e2c86fe47752bcdfb6c471c026bf44c4a2165c72
MadhuBavireddy <madhubavireddy123@gmail.com>
2025-11-11 10:56:22 +0530

    refactor: comment out HashRouter implementation in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter as Router } from "react-router-dom";
+// import { HashRouter as Router } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <Router>
-    <App />
-  </Router>
-);
+createRoot(document.getElementById("root")).render(<App />);
