fe743cffbf07eeb2f048f8b127ea33952778d82f
MadhuBavireddy <madhubavireddy123@gmail.com>
2025-11-11 10:24:45 +0530

    refactor: remove commented-out HashRouter implementation in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,4 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  // <Router>
-  <App />
-  // </Router>
-);
+createRoot(document.getElementById("root")).render(<App />);
